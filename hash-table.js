0069976a318dd34f6a2f24f89d5013c8989c1989
Javier Hernandez <javierLhernandez@yahoo.com>
2023-10-25 18:33:49 -0400

    hashMod

--- hash-table.js
+++ hash-table.js
@@ -12,14 +12,26 @@ class HashTable {
 
   constructor(numBuckets = 4) {
     // Your code here 
+    this.count = 0;
+    this.capacity = numBuckets;
+    this.data = new Array(this.capacity).fill(null)
+
   }
 
   hash(key) {
     // Your code here 
+    let first8 = sha256(key).slice(0, 8)
+    let int = parseInt(first8, 16)
+    // console.log(int)
+    return int
   }
 
   hashMod(key) {
     // Your code here 
+    return this.hash(key) % this.capacity
+    // OR
+    // return this.hash(key) % this.data.length()
+
   }
 
   insertNoCollisions(key, value) {
